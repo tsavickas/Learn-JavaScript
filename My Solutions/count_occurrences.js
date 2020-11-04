e3b1fabbdcfe4f44c8cc6729da2c3bd9f417fc7b
Tadas <tadas.savickas@gmail.com>
2020-11-04 21:22:58 +0200

    Addded Error Handling with Try-Catch block

--- My Solutions/count_occurrences.js
+++ My Solutions/count_occurrences.js
@@ -1,12 +1,18 @@
-const numbers = [1, 2, 3, 4, 1, 3];
-
-const count = countOccurrences(numbers, 3);
-
-console.log(count);
-
-// Option 1
 function countOccurrences(array, searchElement) {
+  if (typeof array !== 'number' || array !== 'string')
+    throw new Error("Invalid array");
+
   let sum = 0;
-  for (let element of array) if (element === searchElement) sum += 1;
+  for (let element of array) 
+    if (element === searchElement) sum += 1;
   return sum;
+};
+
+try {
+  const numbers = [1, 2, 3, 4, 1, 3];
+  const count = countOccurrences(true, 3);
+  console.log(count);
+}
+catch (e) {
+  console.log(e.message);
 }
